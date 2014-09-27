3e50172030241e2e58d7bd592430dc01ac3dacf9
jameshruby <windstormer4@gmail.com>
2014-09-27 03:19:06 -0700

    d3d

--- Ch01_01EmptyProject/D3DClass.cs
+++ Ch01_01EmptyProject/D3DClass.cs
@@ -24,28 +24,113 @@ namespace Ch01_01EmptyProject
 
         public void InitializeWithForm(Form1 form)
         {
-            //var highestFL = Device.GetSupportedFeatureLevel();
-            //if (highestFL <  FeatureLevel.Level_11_0)
-            //{ 
-            //    throw new NotSupportedException();
-            //}
+            var deviceFlags = DeviceCreationFlags.None;
+
+            bool enforceSoftwareRendering = false;
+            DriverType driverType = GetDriverTypeForRenderingWhichSupportsDx11(enforceSoftwareRendering);
+
+#if DEBUG
+            deviceFlags = DeviceCreationFlags.Debug;
+#endif
+            try
+            {
+                device = new Device(driverType, deviceFlags);
+            }
+            catch (Exception)
+            {
+                //MessageBox(0, L"Direct3D Feature Level 11 unsupported.", 0, 0);
+                throw;
+            }
+            //CHECK AntiAliasing quality suport code coud be here
+
 
             ModeDescription modeDescription = CreateModeDescription(form);
-            var swapChainSetup = SetUpSwapChain(form, ref modeDescription);
-            // Used for debugging dispose object references
-            // Configuration.EnableObjectTracking = true;
+            SwapChainDescription swapChainDescription = SwapChainDescription(form, ref modeDescription);
 
-            // Disable throws on shader compilation errors - hopefully
-            // we won't have any of these! :)
-            //Configuration.ThrowOnShaderCompileError = false;
+            //CreateSwapChain(swapChainDescription);
 
-            CreateSwapChain(swapChainSetup);
+            using (var dxgi = device.QueryInterface<SharpDX.DXGI.Device>())
+            using (var adapter = dxgi.Adapter)
+            using (var factory = adapter.GetParent<Factory>())
+            {
+                swapChain = new SwapChain(factory, device, swapChainDescription);
+            }
 
+            //CreateResourceViewFromBackBuffer();
+            //RenderToBackBuffer();
+
+            backBuffer = Texture2D.FromSwapChain<Texture2D>(swapChain, 0);
+            // Create an RTV for the rendertarget.
+            renderTargetView = (new RenderTargetView(device, backBuffer));
+
+            var depthBuffer = new Texture2DDescription();
+            depthBuffer.Format = Format.D24_UNorm_S8_UInt;
+            depthBuffer.ArraySize = 1;
+            depthBuffer.MipLevels = 1;
+            depthBuffer.Width = form.ClientSize.Width;
+            depthBuffer.Height = form.ClientSize.Height;
+
+            // no MSAA
+            depthBuffer.SampleDescription.Count = 1;
+            depthBuffer.SampleDescription.Quality = 0;
+
+            depthBuffer.Usage = ResourceUsage.Default;
+            depthBuffer.BindFlags = BindFlags.DepthStencil;
+            depthBuffer.CpuAccessFlags = 0;
+            depthBuffer.OptionFlags = 0;
+
+            Texture2D DepthBuffer = new Texture2D(device, new
+ Texture2DDescription()
+            {
+                Format = SharpDX.DXGI.Format.D24_UNorm_S8_UInt,
+                ArraySize = 1,
+                MipLevels = 1,
+                Width = (int)form.ClientSize.Width,
+                Height = (int)form.ClientSize.Height,
+                SampleDescription = swapChain.Description.SampleDescription,
+                BindFlags = BindFlags.DepthStencil,
+            });
             var context = device.ImmediateContext;
+            var DepthStencilView = new DepthStencilView(
+device,
+DepthBuffer,
+new DepthStencilViewDescription()
+{
+    Dimension =
+    (swapChain.Description.SampleDescription.Count > 1 ||
+    swapChain.Description.SampleDescription.Quality > 0)
+    ? DepthStencilViewDimension.Texture2DMultisampled :
+    DepthStencilViewDimension.Texture2D
+});
+            // Set the OutputMerger targets
+            context.OutputMerger.SetTargets(DepthStencilView,
+            renderTargetView);
 
-            CreateResourceViewFromBackBuffer();
 
-            RenderToBackBuffer();
+
+            //var depthStencilBuffer = new Texture2D(device, depthBuffer);
+            //DepthStencilView depthStencilView = new DepthStencilView(device, depthStencilBuffer);
+
+
+            ////Set OutputMerger targets
+            //context.OutputMerger.SetTargets(depthStencilView, renderTargetView);
+
+
+
+
+        }
+
+        private static DriverType GetDriverTypeForRenderingWhichSupportsDx11(bool enforceSoftwareRendering)
+        {
+            FeatureLevel highestDirectXVersionSupported = Device.GetSupportedFeatureLevel();
+            if (highestDirectXVersionSupported < FeatureLevel.Level_11_0 && enforceSoftwareRendering)
+            {
+                return DriverType.Software;
+            }
+            else
+            {
+                return DriverType.Hardware;
+            }
         }
 
         public void StartRender(Form1 form)
@@ -60,7 +145,7 @@ namespace Ch01_01EmptyProject
 
         private void BindBuffersToOutputStageOfPipeline()
         {
-            Viewport v = new Viewport(0, 0, 40, 40);
+            //Viewport v = new Viewport(0, 0, 40, 40);
             device.ImmediateContext.ClearRenderTargetView(renderTargetView, Color.Blue);
         }
 
@@ -93,7 +178,7 @@ namespace Ch01_01EmptyProject
             return modeDescription;
         }
 
-        private static SwapChainDescription SetUpSwapChain(Form1 form, ref ModeDescription modeDescription)
+        private static SwapChainDescription SwapChainDescription(Form1 form, ref ModeDescription modeDescription)
         {
             // SwapChain description: set up our double buffer
             var swapChainSetup = new SwapChainDescription();
