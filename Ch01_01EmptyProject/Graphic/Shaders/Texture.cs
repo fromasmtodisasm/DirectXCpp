f6e2c5169e1a2dd9425ae64bc97644b4b24e3a2c
jameshruby <windstormer4@gmail.com>
2014-10-20 01:52:42 -0700

    texture added

---
+++ Ch01_01EmptyProject/Graphic/Shaders/Texture.cs
@@ -0,0 +1,29 @@
+﻿using SharpDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch01_01EmptyProject.Graphic.Shaders
+{
+    class Texture
+    {
+        private ShaderResourceView textureResource;
+
+        public static ShaderResourceView Load(Device device)
+        {
+            string textureFileName = @"D:\Github\DirectXCpp\Ch01_01EmptyProject\Graphic\Shaders\Textures\wall.dds";
+
+            try
+            {
+             return ShaderResourceView.FromFile(device, textureFileName);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("textureLoad: " + ex);
+            }
+        }
+        
+    }
+}
